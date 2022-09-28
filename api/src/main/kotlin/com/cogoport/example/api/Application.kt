3318835b934371f78305596a21ad32c663dc1f89
Amanjeet Kumar <108551871+AmanjeetCogoport@users.noreply.github.com>
2022-09-28 11:30:18 +0530

    Initial commit

---
+++ api/src/main/kotlin/com/cogoport/example/api/Application.kt
@@ -0,0 +1,9 @@
+package com.cogoport.example.api
+import io.micronaut.runtime.Micronaut.build
+fun main(args: Array<String>) {
+    build()
+        .args(*args)
+        .packages("com.cogoport.example.api")
+        .start()
+    println("hey")
+}
